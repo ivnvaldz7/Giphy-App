5b718d35e6530d15a8bb50220144d400bcc73e7a
ivnvaldz7 <ivnvaldz@gmail.com>
2023-01-22 22:26:56 -0300

    22/01

--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 
 export const AddCategory = ({onAddCategories}) => {
-  console.log(AddCategory);
   const [inputValue, setInputValue] = useState('');
   const inputChange = ({target})=>{
     setInputValue(target.value) 
@@ -9,7 +8,7 @@ export const AddCategory = ({onAddCategories}) => {
   const onSubmit = e =>{
     e.preventDefault()
     onAddCategories(categories=>[inputValue,...categories])
-    console.log(inputValue);
+    setInputValue('')
   } 
     
   return (
